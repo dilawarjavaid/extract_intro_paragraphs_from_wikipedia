e28c50879c993bcb793f75e874ea54134c08f0d4
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-09 09:15:08 -0700

    Check that the response is valid, then parse the HTML content.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import bs4
 import requests
+
+#Make the HTTP Request
+response = requests.get("https://en.wikipedia.org/wiki/Mathematics")
+
+
+#Parse the HTML
+if response is not None:
+    html = bs4.BeautifulSoup(response.text, 'html.parser')
