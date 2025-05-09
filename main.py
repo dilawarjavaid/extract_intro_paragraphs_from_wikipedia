9b700a4b384ed68677acb889c0095e24d9ab5fad
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-09 09:16:06 -0700

    Use select() to get the title and the first few paragraphs.

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import requests
 #Make the HTTP Request
 response = requests.get("https://en.wikipedia.org/wiki/Mathematics")
 
-
 #Parse the HTML
 if response is not None:
     html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+#Extract Page Title and Paragraphs
+title = html.select("#firstHeading")[0].text
+paragraphs = html.select("p")
+for para in paragraphs:
+    print(para.text)
